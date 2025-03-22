7ed194468d88dc9d6eb2eed60904f025aa27453a
SusmithaGandla <susmitha2735@gmail.com>
2025-03-22 23:22:27 +0530

    Add files via upload

---
+++ src/pages/admin-view/orders.jsx
@@ -0,0 +1,17 @@
+// function AdminOrders(){
+//     return <div>admin orders</div>  
+// }
+
+// export default AdminOrders;
+
+import AdminOrdersView from "@/components/admin-view/orders";
+
+function AdminOrders() {
+  return (
+    <div>
+      <AdminOrdersView />
+    </div>
+  );
+}
+
+export default AdminOrders;
